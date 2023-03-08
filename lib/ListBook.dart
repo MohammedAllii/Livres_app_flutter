ec3ef1f2327ceea83fd1b82091b73adeacc91090
Mohamed ali <mejrihamouda8@gmail.com>
2023-03-08 11:58:48 +0100

    first commit

---
+++ lib/ListBook.dart
@@ -0,0 +1,58 @@
+import 'Book.dart';
+
+List<Book> books = [
+   Book(
+      title: 'Game of thrones',
+      author: 'George R.R. Martin ',
+      description: 'A Game of Thrones est le premier livre de la saga Le Trône de fer écrite par George R. R. Martin. Le livre, d"ambiance médiévale, a été publié en version originale en 1996, puis en version française de 1998 à 1999. Il a remporté le prix Locus du meilleur roman de fantasy en 1997'
+      ,imageUrl: 'https://m.media-amazon.com/images/I/61o6pfHHU-L._SX636_BO1,204,203,200_.jpg',id: 1, price: 120,quantity:1)
+  ,
+   Book(
+      title: 'Viking',
+      author: 'Alexander Aaronsohn',
+      description: 'Perl is a powerful programming language that has grown in popularity since it first appeared in 1988.'
+      ,imageUrl: 'https://images.penguinrandomhouse.com/cover/9781785858598',id: 2, price: 520,quantity:1)
+  ,
+  Book(
+      title: 'East of Eden by John Steinbeck',
+      author: 'Gjorgji Abadžiev',
+      description: 'Jules is a young man barely a century old. He"s lived long enough to see the cure for death and the end of scarcity',
+      imageUrl: 'https://static.vecteezy.com/system/resources/previews/005/711/871/original/icons-of-books-a-flat-design-style-books-in-a-stack-open-in-a-group-closed-on-the-shelf-reading-learn-and-receive-education-through-books-free-vector.jpg',id: 3, price: 1100,quantity:1),
+
+      
+  Book(
+      title: 'The House of Mirth by Edith Wharton',
+      author: 'Jonathan Aaron',
+      description: 'Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon.',
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/5606/5606075.png',id: 4, price: 202,quantity:1),
+Book(
+      title: 'The Man Who Was Thursday by G.K. Chesterton',
+      author: 'Abe Akira',
+      description: 'Lost : Les Disparus (Lost) — ou Perdus au Québec — est un feuilleton télévisé dramatique américain diffusé du 22 septembre 2004 au 23 mai 2010 sur le réseau ABC. Créée par J. J. Abrams, Damon Lindelof et Jeffrey Lieber, elle s"étend sur six saisons, comprenant un total de 121 épisodes',
+      imageUrl: 'https://m.media-amazon.com/images/M/MV5BNzhlY2E5NDUtYjJjYy00ODg3LWFkZWQtYTVmMzU4ZWZmOWJkXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg',id: 5, price: 2147,quantity:1),
+      Book(
+      title: 'Are You There, Vodka? It"s Me, Chelsea by Chelsea Handler',
+      author: 'Antoine Abel',
+      description: 'Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon.',
+      imageUrl: 'https://cdn1.vectorstock.com/i/1000x1000/89/70/closed-book-cartoon-symbol-icon-design-beautiful-vector-23998970.jpg',id: 6, price: 407,quantity:1),
+      Book(
+      title: 'And Then There Were None by Agatha Christie',
+      author: 'Lascelles Abercrombie',
+      description: 'Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon.',
+      imageUrl: 'https://img.freepik.com/premium-vector/book-cartoon_22350-95.jpg?w=2000',id: 7, price: 80,quantity:1),
+      Book(
+      title: 'Their Eyes Were Watching God by Zora Neale Hurston',
+      author: 'Chris van Abkoude',
+      description: 'Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon.',
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/207/207114.png',id: 8, price: 308,quantity:1),
+      Book(
+      title: 'The Devil Wears Prada by Lauren Weisberger',
+      author: 'Mena Kasmiri Abdullah',
+      description: 'Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon.',
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/6515/6515392.png',id: 9, price: 50,quantity:1),
+      Book(
+      title: 'Brave New World by Aldous Huxley',
+      author: 'Kader Abdolah',
+      description: 'Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon.',
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2780/2780068.png',id: 10, price: 1300,quantity:1),
+];
